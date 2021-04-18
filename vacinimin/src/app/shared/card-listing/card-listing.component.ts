89f755cb4c1d39b21d334f89787753331e48f41a
thalytabdn <thalyta.nascimento@ccc.ufcg.edu.br>
2021-04-17 23:52:02 -0300

    tela de listagem completa :white_check_mark:

--- vacinimin/src/app/shared/card-listing/card-listing.component.ts
+++ vacinimin/src/app/shared/card-listing/card-listing.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-listing',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-listing.component.css']
 })
 export class CardListingComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() subtitle: string;
+  @Input() info1: string;
+  @Input() info2: string;
 
   constructor() { }
 
--- vacinimin/src/app/standart/standart/standart.component.ts
+++ vacinimin/src/app/standart/standart/standart.component.ts
@@ -8,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class StandartComponent implements OnInit {
 
   title = 'Postos de vacinação';
+  // lista com informações dos postos
+  listPosts = [
+    {
+      "nome": "Campestre",
+      "endereco": "Rua ipsum Lorem dolor",
+      "cep": "584511-000",
+      "disponivel": true
+    },
+    {
+      "nome": "Campestre",
+      "endereco": "Rua Lorem ipsum dolor",
+      "cep": "584511-000",
+      "disponivel": true
+    },
+    {
+      "nome": "Alto Branco",
+      "endereco": "Rua Lorem ipsum dolor",
+      "cep": "584511-080",
+      "disponivel": false
+    },
+    {
+      "nome": "Cruzeiro",
+      "endereco": "Rua 3",
+      "cep": "584511-080",
+      "disponivel": false
+    }
+  ];
+
+  pathCheck = "../../../assets/icons/vaccineCheck.svg";
+  pathNotCheck = "../../../assets/icons/RedFlag.svg";
 
   constructor() { }
 
